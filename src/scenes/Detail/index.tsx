69ec5714b48416749f3e29b1f5efde68b1369a33
Daniel <daniel.rod.moreno@gmail.com>
2025-02-24 22:49:31 +0100

    feat: add rating display to movie detail page

--- src/scenes/Detail/index.tsx
+++ src/scenes/Detail/index.tsx
@@ -1,20 +1,21 @@
 import { MovieDetail } from '@/logic/movies/schema';
 import { BreadCrumb } from 'primereact/breadcrumb';
+import { Rating } from 'primereact/rating';
 
 type Props = {
     movie: MovieDetail;
 };
 
 export default function Detail({ movie }: Props) {
-    const iconItemTemplate = () => {
-        return <span>Detail</span>;
-    };
+    const ratingValue = +movie.imdbRating / 2;
+
     return (
         <div className="flex flex-col">
             <BreadCrumb
-                model={[{ template: iconItemTemplate }]}
+                model={[{ template: <span>Detail</span> }]}
                 home={{ icon: 'pi pi-home', url: '/' }}
             />
+            <Rating value={ratingValue} readOnly cancel={false} />
             DETAIL:{movie.title}{' '}
         </div>
     );
