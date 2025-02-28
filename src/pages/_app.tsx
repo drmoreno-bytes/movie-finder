daa5b41ea18fd2c4be067678209e85aa6638d56f
Daniel <daniel.rod.moreno@gmail.com>
2025-02-28 11:04:30 +0100

    feat: integrate Redux store and remove unused mock data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import '@/styles/globals.css';
 import 'primeicons/primeicons.css';
 import type { AppProps } from 'next/app';
 import Tailwind from 'primereact/passthrough/tailwind';
+import { store } from '@/store';
+import { Provider } from 'react-redux';
 
 import { PrimeReactProvider } from 'primereact/api';
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
-            <Component {...pageProps} />
-        </PrimeReactProvider>
+        <Provider store={store}>
+            <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+                <Component {...pageProps} />
+            </PrimeReactProvider>
+        </Provider>
     );
 }
