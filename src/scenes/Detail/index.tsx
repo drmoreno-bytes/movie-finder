f1b912e53f7e8e2a08039529f68cfbc2910a4183
Daniel <daniel.rod.moreno@gmail.com>
2025-03-02 22:55:46 +0100

    feat: enhance Detail and Favorites components with improved layout and styling

--- src/scenes/Detail/index.tsx
+++ src/scenes/Detail/index.tsx
@@ -1,5 +1,8 @@
+import { BackgroundCover } from '@/components/BackgroundCover';
+import { Like } from '@/components/like';
+import { Thumbnails } from '@/components/Thumbnails';
 import { Movie } from '@/logic/movies/schema';
-import { BreadCrumb } from 'primereact/breadcrumb';
+import { Badge } from 'primereact/badge';
 import { Rating } from 'primereact/rating';
 
 type Props = {
@@ -7,16 +10,40 @@ type Props = {
 };
 
 export default function Detail({ movie }: Props) {
-    //const ratingValue = +movie?.imdbRating / 2;
+    const { title, poster, year, type } = movie;
 
     return (
-        <div className="flex flex-col">
-            <BreadCrumb
-                model={[{ template: <span>Detail</span> }]}
-                home={{ icon: 'pi pi-home', url: '/' }}
-            />
-            <Rating value={5} readOnly cancel={false} />
-            DETAIL:{movie.title}{' '}
+        <div className="overflow-hidden relative">
+            <BackgroundCover
+                src={poster}
+                alt={title}
+                width={2048}
+                height={1152}
+                className="opacity-10 block absolute w-full h-full max-h-[980px] after:absolute after:left-0 after:top-0 after:h-full after:w-full after:bg-gradient-to-t after:from-[#0D0D0D] after:to-transparent after:content-[''] top-0 after:pointer-events-none"
+            >
+                <div className="px-5 pt-9 max-w-[800px] mx-auto w-full">
+                    <div className="flex flex-col gap-6 sm:flex-row">
+                        <div className="w-[432] h-[648] mx-auto sm:mx-0">
+                            <Thumbnails
+                                url={`/detail/${movie.id}`}
+                                {...movie}
+                                width={432}
+                                height={648}
+                                actions={<Like movie={movie} />}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <h2 className="uppercase">{title}</h2>
+                            <p className="text-sm">{year}</p>
+                            <Badge
+                                className="bg-[#d80286] max-w-20 uppercase"
+                                value={type}
+                            ></Badge>
+                            <Rating value={5} readOnly cancel={false} />
+                        </div>
+                    </div>
+                </div>
+            </BackgroundCover>
         </div>
     );
 }
--- src/scenes/Favorites/index.tsx
+++ src/scenes/Favorites/index.tsx
@@ -1,5 +1,4 @@
 import { ListOfMovies } from '@/components/ListOfMovies';
-import { MovieBadge } from '@/components/MovieBadge';
 import { useAppSelector } from '@/store/hooks/useAppSelector';
 import { useEffect, useState } from 'react';
 
@@ -16,11 +15,13 @@ export default function Favorites() {
     }
 
     return (
-        <div className="flex flex-col max-w-5xl justify-center mx-auto pt-12 pb-32">
-            <ListOfMovies
-                movies={favoriteMovies}
-                total={favoriteMovies.length ?? 0}
-            />
+        <div className="overflow-hidden relative">
+            <div className="flex flex-col max-w-5xl justify-center mx-auto pt-12 pb-32">
+                <ListOfMovies
+                    movies={favoriteMovies}
+                    total={favoriteMovies.length ?? 0}
+                />
+            </div>
         </div>
     );
 }
